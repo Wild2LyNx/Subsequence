ffecdf540516bb30820ae9626d66409759f50f39
Lilia <lilek_rs@bk.ru>
2012-08-18 19:19:20 +0400

    Just skeleton of implementation.

---
+++ Subsequence/src/com/tsystems/javaschool/tasks/SubsequenceImpl.java
@@ -0,0 +1,20 @@
+package com.tsystems.javaschool.tasks;
+
+import java.util.List;
+
+public class SubsequenceImpl implements Subsequence {
+
+	/* 
+	 * @see com.tsystems.javaschool.tasks.Subsequence#find(java.util.List, java.util.List)
+	 */
+	@Override
+	@SuppressWarnings("rawtypes")
+	public boolean find(List x, List y) { 
+		Object a;
+		Object b;
+		a.super.
+		x.removeAll(y);
+		return false;
+	}
+
+}
