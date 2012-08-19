82148e5fd76f153568e342481c36211b730ec869
Lilia <lilek_rs@bk.ru>
2012-08-19 16:13:56 +0400

    Handle of some exceptions and accrue of some tests.

--- Subsequence/src/com/tsystems/javaschool/tasks/SubsequenceImpl.java
+++ Subsequence/src/com/tsystems/javaschool/tasks/SubsequenceImpl.java
@@ -2,26 +2,47 @@ package com.tsystems.javaschool.tasks;
 
 import java.util.List;
 
+/**
+ * Checks if it is possible to get a sequence which is equal to the first one by
+ * removing some elements from the second one.
+ * 
+ * If one of the two sequences is null, and the second - not null, then it
+ * returns false.
+ * 
+ * @param x
+ *            first sequence
+ * @param y
+ *            second sequence
+ * @return <code>true</code> if possible, otherwise <code>false</code>
+ * 
+ * @see com.tsystems.javaschool.tasks.Subsequence#find(java.util.List,
+ *      java.util.List)
+ */
+
 public class SubsequenceImpl implements Subsequence {
 
-	/*
-	 * @see com.tsystems.javaschool.tasks.Subsequence#find(java.util.List,
-	 * java.util.List)
-	 */
 	@Override
 	@SuppressWarnings("rawtypes")
 	public boolean find(List x, List y) {
+		if ((x == null) | (y == null))
+			return false;
 		if (x.size() > y.size())
 			return false;
-		int j = 0;
+		int j = 0; //counter for elements of first sequence 
 		for (int i = 0; i < y.size(); i++) {
-			if ((j < x.size()) && (x.get(j).equals(y.get(i))))
-				j++;
+			if ((j < x.size())) {
+				if ((x.get(j) == null) && (y.get(i) == null))
+					j++;
+				else if (x.get(j) != null) {
+					if (x.get(j).equals(y.get(i)))
+						j++;
+				}
+
+			}
 		}
 		if (j == x.size())
 			return true;
 		else
 			return false;
 	}
-
 }
--- Subsequence/src/com/tsystems/javaschool/tasks/Testing.java
+++ Subsequence/src/com/tsystems/javaschool/tasks/Testing.java
@@ -20,6 +20,14 @@ public class Testing {
 		boolean b2 = s.find(Arrays.asList("A", 5454, "C", "D"),
 				Arrays.asList(5678, "A", "AB", "B", "M", "D", "C", 5454, "D"));
 		System.out.println(b2); // Result: false
+
+		boolean b3 = s.find(null, Arrays.asList("A", 42, "C", null));
+		System.out.println(b3); // Result: false
+
+		boolean b4 = s.find(Arrays.asList(98, null, -56, "brr"), Arrays
+				.asList("alpha", 98, "beta", "gamma", null, 849, -87, -56,
+						"shh", "brr"));
+		System.out.println(b4); // Result: true
 	}
 
 }
